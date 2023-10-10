146b81507bb31bd7d9ea1987c99a0179deae6807
Ajay Kumar B <b.ajaykumar306k@gmail.com>
2023-10-10 22:15:34 +0530

    deploy demo

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,11 +2,17 @@ import { Button, Stack, TextField, Typography } from '@mui/material'
 import React, { useState } from 'react'
 
 function Login() {
+    const [fields, setfields] = useState({
+        fieldusername : '',
+        fieldemail : '',
+        fieldpassword : ''
+    })
     const [user, setUser] = useState({
         username : '',
-        password : ''
+        password : '',
+        name : ''
     })
-    const { username, password } = user;
+    const { username, name, password } = user;
     function changeHandler(e) {
         setUser({
             ...user,
@@ -16,13 +22,14 @@ function Login() {
     function submitHandler(e) {
         e.preventDefault();
         console.log(user);
-        setUser({username : '', password : ''})
+        alert(`user ${username} ${name} and ${password}`)
+        setUser({username : '', name : '', password : ''})
     }
   return (
     <>
     <hr />
         <div className='d-flex justify-content-around pt-4'>
-            <div class="card" style={{width: '40rem'}}>
+            <div className="card" style={{width: '40rem'}}>
                 <img src="https://res.cloudinary.com/dbie5v8ke/image/upload/v1696695939/WhatsApp_Image_2023-10-07_at_9.53.45_PM_rvlhqy.jpg" className="card-img-top" alt="loading!" />   
             </div>
             <div className="card mt-5 pt-5 border border-0" style={{width: '40%'}}>
@@ -30,16 +37,21 @@ function Login() {
                 <form onSubmit={submitHandler}>
                     <Stack spacing={2}>
                         <Stack spacing={1}>
-                            <Typography variant='h4' className='fw-bolder' style={{ letterSpacing : '3px'}}>Log in to Exclusive</Typography>
+                            <Typography variant='h5' className='fw-bolder' style={{ letterSpacing : '1px'}}>Create an account</Typography>
                             <Typography variant='subtitle2' className='fw-bold'>Enter your details below</Typography>
                         </Stack>
                         <Stack spacing={2}>
-                            <TextField label='Email or Phone Number' variant='standard' name='username' value={username} onChange={changeHandler} />
+                            <TextField label='name' variant='standard' value={username}  name='username' onChange={changeHandler} />
+                            <TextField label='Email or Phone Number' variant='standard' name='name' value={name} onChange={changeHandler} />
                             <TextField label='Password' variant='standard' name='password' value={password} onChange={changeHandler} />
                         </Stack>
-                        <Stack display='block'>
-                            <Button variant='contained' type='submit' color='error' className='px-4 py-2'>Log In</Button>
-                            <Typography style={{ float : 'right'}} color='error' variant='h6'>Forgot Password ?</Typography>
+                        <Stack spacing={2} className='mt-4 pt-2'>
+                            <Button variant='contained' type='submit' id='color'>Create an account</Button>
+                            <button> <span><img src='https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png' alt='...' /></span>Sign up with Google</button>
+                        </Stack>
+                        <Stack spacing={2} direction='row' className='ps-5 ms-5'>
+                            <Typography variant='subtitle1' className='pt-1'>Already have account ?</Typography>
+                            <Button variant='text' className='text-dark'>Login</Button>
                         </Stack>
                     </Stack>
                 </form>
